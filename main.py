0f73337c012148d266bb024eceac1b681098872b
ieunune <une@kakao.com>
2022-11-01 23:55:21 +0900

    [feat] requests, BeautifulSoup 이용 스크래퍼 만드는 중

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from requests import get
+from bs4 import BeautifulSoup
 
-websites = ("google.com","airbnb.com","https://twitter.com","facebook.com","https://tictok.com")
+# websites = ("google.com","airbnb.com","https://twitter.com","facebook.com","https://tictok.com")
+#
+# for website in websites:
+#     if not website.startswith("https://") :
+#         website = f"https://{website}"
+#     print(website)
 
-for website in websites:
-    if not website.startswith("https://") :
-        website = f"https://{website}"
-    print(website)
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs")
+    # print(len(jobs))
+    for job_section in jobs:
+        # print(job_section.find_all('li'))
+        job_posts = job_section.find_all('li')
+        job_posts.pop(1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            # print(anchors)
+            anchor = anchors[1]
+            # print('href : ' + anchor['href'])
+            company, kind, region = anchor.find_all('span', class_="company")
+            # print(company, kind, region)
+            title = anchor.find('span', class_='title')
+            print(company, kind, region, title)
+            0
+
